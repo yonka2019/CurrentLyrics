f8755471028bb6bf113f2bfd3cb0a8bace1df297
Yonka 2019 <yonka2003@gmail.com>
2021-08-15 02:24:20 +0300

    release

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ SPOTIFY_REFRESH_TOKEN = "-"
 spotify_auth_info = dict(
 	CLIENT_ID='-',
 	CLIENT_SECRET='-',
-	REDIRECT_URI='https://google.co.il'
+	REDIRECT_URI='-'
 )
 
 access_tokens = dict(
-	ACCESS_TOKEN_GENIUS='KqPm_maXDj8OaCd1_a0MKx3TCleHg8oW8BEnYgJq2MSbBjJ7xdltLI3qHatAGc7y',
+	ACCESS_TOKEN_GENIUS='-',
 	ACCESS_TOKEN_SPOTIFY=SpotifyAuth.get_auth_token(SPOTIFY_REFRESH_TOKEN)
 )
